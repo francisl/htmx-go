90a8764badd6af6e3691253385a8eaf35ec07651
Francis Lavoie <flavoie@ferlab.bio>
2024-02-21 15:37:36 -0500

    add example with hx-swap

--- server.go
+++ server.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"htmx-go-intro/utils"
 	"htmx-go-intro/views"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 type State struct {
@@ -12,8 +13,6 @@ type State struct {
 
 func main() {
 	e := echo.New()
-	e.HTTPErrorHandler = utils.CustomHTTPErrorHandler
-
 	e.Static("/public", "public")
 
 	state := State{count: 0}
