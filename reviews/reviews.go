1acf7b60b8cca4ed9a9506ab8c6260ed3d96c814
Francis Lavoie <flavoie@ferlab.bio>
2024-02-22 00:19:08 -0500

    Form with tables update

---
+++ reviews/reviews.go
@@ -0,0 +1,104 @@
+package reviews
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Review struct {
+	Id           int       `form:"id" json:"id"`
+	Presentation string    `form:"presentation" json:"presentation" binding:"required"`
+	Rating       int       `form:"rating" json:"rating" binding:"required"`
+	TheGood      string    `form:"good" json:"good" binding:"required"`
+	TheBad       string    `form:"bad" json:"bad" binding:"required"`
+	Presenter    string    `form:"presenter" json:"presenter" binding:"required"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type CreateReview struct {
+	Presentation string `form:"presentation" json:"presentation" binding:"required"`
+	Rating       string `form:"rating" json:"rating" binding:"required"`
+	TheGood      string `form:"good" json:"good" binding:"required"`
+	TheBad       string `form:"bad" json:"bad" binding:"required"`
+	Presenter    string `form:"presenter" json:"presenter" binding:"required"`
+}
+
+var reviews []Review = []Review{
+	{
+		Id:           0,
+		Presentation: "First Review",
+		Rating:       4,
+		TheGood:      "Talked about the good stuff.",
+		TheBad:       "Didn't talk about tailwindcss.",
+		Presenter:    "John Doe",
+		CreatedAt:    time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		Id:           1,
+		Presentation: "Second Review",
+		Rating:       5,
+		TheGood:      "Was very informative.",
+		TheBad:       "Didn't talk about tailwindcss.",
+		Presenter:    "Jane Doe",
+		CreatedAt:    time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		Id:           2,
+		Presentation: "Third Review",
+		Rating:       5,
+		TheGood:      "Wow, just realised I can build web application without react.",
+		TheBad:       "No word on tailwindcss.",
+		Presenter:    "Foo Bar",
+		CreatedAt:    time.Date(2024, 2, 3, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		Id:           4,
+		Presentation: "Fourth Review",
+		Rating:       2,
+		TheGood:      "is this required?",
+		TheBad:       "Really, tailwindcss and fp programming are not mentioned.",
+		Presenter:    "Lorem Ipsum",
+		CreatedAt:    time.Date(2024, 2, 3, 0, 0, 0, 0, time.UTC),
+	},
+}
+
+func All() []Review {
+	return reviews
+}
+
+func Add(n CreateReview) (Review, error) {
+	intRating, err := strconv.Atoi(n.Rating)
+	if err != nil {
+		return Review{}, fmt.Errorf("cannot convert id %s ", n.Rating)
+	}
+
+	review := Review{
+		Id:           len(reviews),
+		Presentation: n.Presentation,
+		Rating:       intRating,
+		TheGood:      n.TheGood,
+		TheBad:       n.TheBad,
+		Presenter:    n.Presenter,
+		CreatedAt:    time.Now(),
+	}
+	reviews = append(reviews, review)
+
+	return review, nil
+}
+
+func Delete(id string) error {
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		return fmt.Errorf("cannot convert id %d ", intID)
+	}
+
+	for idx, review := range reviews {
+		if review.Id == intID {
+			reviews = append(reviews[:idx], reviews[idx+1:]...)
+			return nil
+		}
+
+	}
+	return fmt.Errorf("there is no review  %d ", intID)
+}
--- server.go
+++ server.go
@@ -1,23 +1,48 @@
 package main
 
 import (
+	"htmx-go-intro/reviews"
 	"htmx-go-intro/views"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
-
-type State struct {
-	count int
-}
 
 func main() {
 	e := echo.New()
 	e.Static("/public", "public")
 
-	state := State{count: 0}
-	e.GET("/ticket", func(c echo.Context) error {
-		component := views.Ticket(state.count)
-		state.count++
+	e.GET("/reviews", func(c echo.Context) error {
+		component := views.Reviews(reviews.All())
+		return component.Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.DELETE("/reviews/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		err := reviews.Delete(id)
+
+		if err != nil {
+			e.Logger.Error(err)
+			return c.String(http.StatusNotFound, "Not Found")
+		}
+
+		return c.HTML(http.StatusOK, "")
+	})
+
+	e.GET("/reviewform", func(c echo.Context) error {
+		component := views.ReviewForm()
+		return component.Render(c.Request().Context(), c.Response().Writer)
+	})
+
+	e.POST("/reviewform", func(c echo.Context) error {
+		review, _ := reviews.Add(reviews.CreateReview{
+			Presentation: c.FormValue("presentation"),
+			Rating:       c.FormValue("rating"),
+			TheGood:      c.FormValue("good"),
+			TheBad:       c.FormValue("bad"),
+			Presenter:    c.FormValue("presenter"),
+		})
+		component := views.ReviewFormPost(review)
 		return component.Render(c.Request().Context(), c.Response().Writer)
 	})
 
